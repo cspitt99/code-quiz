30c0294f4dfaca4b3ac5174f4b77faee393e122f
cspitt99 <104328339+cspitt99@users.noreply.github.com>
2022-10-13 18:43:31 -0400

    updated quiz.js and attempted to fix bugs

--- develop/quiz.js
+++ develop/quiz.js
@@ -1,35 +1,38 @@
-//container selectors 
-let startPageContainerEl = document.querySelector("#start-page-container");
-let quizContainerEl = document.querySelector("#quiz-container");
-let summaryContainerEl = document.querySelector("#summary-container");
-let highscoreContainerEl = document.querySelector("#highscore-page");
+let startPageEl = document.querySelector("#start-page");
+let quizPageEl = document.querySelector("#quiz-page");
+let endPageEl = document.querySelector("#end-page");
+let highscorePageEl = document.querySelector("#highscore-page");
 
-// timer selector and start page start btn selector (for dynamic header and strtpage element selectors)
 let timerEl = document.querySelector("#time-left");
-let startQuizBtn = document.querySelector("#start-quiz-button");
-let scoreLinkEl = document.querySelector("#highscore-link")
+let startBtn = document.querySelector("#start-btn")
+let scoreLinkEl = document.querySelector("#highscore-page")
 
-// quiz container element selectors
+let questionEl = document.querySelector("#question");
+let choice1El = document.querySelector("#choice1");
+let choice2El = document.querySelector("#choice2");
+let choice3El = document.querySelector("choice3");
+let choice4El = document.querySelector("#choice4");
+let answerListEl = [choice1El, choice2El, choice3El, choice4El];
+let answerStateEl = document.querySelector("#answer-state");
 
+let scoreValEl = document.querySelector("#finalScore");
+let initInput = document.querySelector("#initals-input");
+let saveScoreBtn = document.querySelector("#save-btn");
 
-// summary container element selectors 
+let scoreListEl = document.querySelector("#score-list");
+let homeBtn = document.querySelector("#home-btn");
 
+let initial = localStorage.getItem("inital");
+let score = localStorage.getItem("score");
 
-// highscore page container element selectors
-
-
-// local storage getters
-
-
-// q&a set for quiz
-let questions = [
+const questionSet = [
     {
         question: 'Javascript is an _______ language?',
         choice1: 'Object-Oriented',
         choice2: 'Object-Based',
         choice3: 'Procedural',
         choice4: 'None of the above',
-        answer: '1'
+        answer: choice1El
     },
     {
         question: 'Which of the following keywords is used to define a variable in Javascript?',
@@ -37,7 +40,7 @@ let questions = [
         choice2: 'let',
         choice3: 'Both A and B',
         choice4: 'None of the above',
-        answer: '3'
+        answer: choice3El
     },
 
     {
@@ -46,7 +49,7 @@ let questions = [
         choice2: 'console.log()',
         choice3: 'window.alert()',
         choice4: 'All of the above',
-        answer: '4'
+        answer: choice4El
     },
     {
         question: 'What does the toLocateString() method do in JS?',
@@ -54,7 +57,7 @@ let questions = [
         choice2: 'Returns a parsed string',
         choice3: 'Returns a localized string representation of an object',
         choice4: 'None of the above',
-        answer: '3'
+        answer: choice3El
     },
     {
         question: 'Which function is used to serialize an object into a JSON string in Javascript?',
@@ -62,54 +65,122 @@ let questions = [
         choice2: 'parse()',
         choice3: 'convert()',
         choice4: 'None of the above',
-        answer: '1'
+        answer: choice1El
     }
 ]
 
-// index for questionSet array, timeLeft used for timer, clockid for setInterval
+let index = 0;
+let timeLeft = 75;
+let clockid;
 
+function startQuiz() {
+    startPageEl.classList.add("hide");
+    quizPageEl.classList.remove("hide");
+    clockid = setInterval(countDown, 1000);
+    displayQuestion();
+}
 
-// starts quiz and will use following functions:countDown(), displayQuestion(), nextQuestion(), wrongAnswer()
+function countDown() {
+    if (timeLeft <= 0 && index < questionSet.length - 1) {
+        clearInterval(clockid)
+        timeLeft = 0;
+        displayEnd();
+    } else if (questionSet.length === index) {
+        clearInterval(clockid);
+        displayEnd();
+    } else {
+        timerEl.textContent = timeLeft;
+        timeLeft--;
+    }
+}
 
+function displayQuestion() {
+    questionEl.textContent = questionSet[index].question
+    choice1El.textContent = questionSet[index].choice1
+    choice2El.textContent = questionSet[index].choice2
+    choice3El.textContent = questionSet[index].choice3
+    choice4El.textContent = questionSet[index].choice4
+}
 
-/* triggered when startQuiz button is clicked, used for setInterval parameter
-  function toggles timer off and on depending on such conditions:
-    - if time is <=0 and the questionSet is incomplete, clear timer, timeLeft is now assigned 0, and displaySummary is called
-    - otherwise if the questionSet at current index is equal to the q&a length, clear timer and displaySummary
-    - otherwise, run timer 
-*/
+function nextQuestion() {
+    index++;
+    displayQuestion();
+}
 
+function wrongAnswer() {
+    timeLeft -= 15;
+}
 
-// function to display appropriate questions and answers according to questionSet index
+function answerCheck(event) {
+    if (event.target === questionSet[index].answer) {
+        answerStateEl.textContent = "Correct!"
+        nextQuestion();
+    } else {
+        answerStateEl.textContent = "Wrong"
+        wrongAnswer();
+    }
+}
 
+function displayEnd() {
+    answerStateEl.textContent = "";
+    endPageEl.classList.remove("hide");
+    quizPageEl.classList.add("hide");
+    scoreValEl.textContent = timeLeft;
+    timerEl.textContent = 0;
+}
 
-// 1 of 2 functions used in answerCheck (toggles next question to display)
+let scoreboardCounter = 1;
 
+function setStorage() {
+    let scoreListItem = document.createElement("li")
 
-// 2 of 2 functions used in answerCheck (if answer incorrect, subtract 15s from timer)
+    let scoreVal = scoreValEl.textContent;
+    let initVal = initInput.value;
 
+    localStorage.setItem("initial", initVal);
+    localStorage.setItem("score", scoreVal);
+    scoreListItem.innerText = scoreboardCounter + ". Initials: " + initVal + "\n  Score: " + scoreVal;
+    scoreListEl.append(scoreListItem);
+    scoreboardCounter++;
+}
 
-/* this function states: 
-  - if the element that was clicked on matches the value paired with the answer key, toggle next question and show correct. 
-  - otherwise, display wrong and trigger wrongAnswer function */
+function displayHighScores() {
+    startPagerEl.classList.add("hide");
+    endPageEl.classList.add("hide");
+    quizPageEl.classList.add("hide")
+    highscorePageEl.classList.remove("hide");
+    answerStateEl.textContent = "";
+    clearInterval(clockid);
+}
 
+function saveAndSetScore() {
+    setStorage();
+    displayHighScores();
+}
 
-// resets dynamic text elements answerState and timer to default, and assigns the scoreVal element to the time left at the end of the quiz
+function goHome() {
+    highscorePageEl.classList.add("hide");
+    startPageEl.classList.remove("hide");
+    timeLeft = 75;
+    index = 0;
+}
 
-// creates the list item for initials and score after quiz and appends list item to the scoreList element (also sets localStorage items)
+function clearStorage() {
+    localStorage.removeItem("initial");
+    localStorage.removeItem("score");
+    scoreListEl.textContent = "";
+    scoreboardCounter = 1;
+}
 
+scoreLinkEl.addEventListener("click", displayHighScores)
 
-// when function needs to be called (from any section), applies hide class to all sections except highscore page to prevent bugs. also clears timer
+startBtn.addEventListener("click", startQuiz);
 
+choice1El.addEventListener("click", answerCheck)
+choice2El.addEventListener("click", answerCheck)
+choice3El.addEventListener("click", answerCheck)
+choice4El.addEventListener("click", answerCheck)
 
-// function used for when save button in summary page is clicked (sets storage and adds list item to score list) GOOD
+saveScoreBtn.addEventListener("click", saveAndSetScore);
 
-
-// function called to go back to start page and reset quiz parameter defaults (timer and q&a set index)
-
-
-// removes list items from score list, resets scoreboard counter, and clears local Storage items
-
-
-
-// add event listeners for interactive HTML elements when clicked upon
+homeBtn.addEventListener("click", goHome);
